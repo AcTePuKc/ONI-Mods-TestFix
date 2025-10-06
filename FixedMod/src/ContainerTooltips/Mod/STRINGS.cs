e0ca4aa6a2f8aee84b0df1ff59edb4b63c8adac8
Shteryan Nikolaev <actepukc@gmail.com>
2025-10-06 14:40:58 +0100

    Add Container Tooltips translation template

---
+++ FixedMod/src/ContainerTooltips/Mod/STRINGS.cs
@@ -0,0 +1,17 @@
+using AzeLib;
+
+namespace STRINGS
+{
+    public sealed class CONTAINERTOOLTIPS : AStrings<CONTAINERTOOLTIPS>
+    {
+        public sealed class STATUSITEMS
+        {
+            public sealed class CONTAINERTOOLTIPSTATUSITEM
+            {
+                public static LocString NAME = "Contents";
+                public static LocString TOOLTIP = "Shows the items in internal storage.";
+                public static LocString EMPTY = "None";
+            }
+        }
+    }
+}
